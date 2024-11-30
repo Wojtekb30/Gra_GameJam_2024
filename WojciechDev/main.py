63bc81d9fd558816b93080560396c1ef2c4b9ef7
Wojciech <wojtekb30.player@gmail.com>
2024-11-30 16:43:38 +0100

    test
    Test czy działa ignorowanie __pycache__

--- WojciechDev/main.py
+++ WojciechDev/main.py
@@ -41,7 +41,7 @@ while r.running == True:
     if r.key_pressed(pygame.K_RIGHT):
         r.camera.rotate(0,2,0)
         
-    print(r.camera.forward_vector)
+    #print(r.camera.forward_vector)
     #r.camera.use_mouse_camera_controls(r.window_size_x,r.window_size_y,sensitivity=0.2,sensitivity_factor=1,reverse_horizontally=False,reverse_vertially=False,mouse_cursor_visible=True) 
     
     r.update_display()
